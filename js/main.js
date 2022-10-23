f97ce5c5a34dde832c27af890a095a3718ccd71f
Edgar Valderrama <valderrama311@gmail.com>
2022-10-22 23:19:14 -0500

    Notes & Questions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 const links = [
   {
-    label: "Week1 notes",
+    label: "Week 1 notes",
     url: "week1/index.html"
+  },
+  {
+    label: "Week 2 notes",
+    url: "week2/index.html"
+  },
+  {
+    label: "Week 3 notes",
+    url: "week3/index.html"
+  },
+  {
+    label: "Week 4 notes",
+    url: "week4/index.html"
+  },
+  {
+    label: "Week 5 notes",
+    url: "week5/index.html"
   }
 ]
 
---
+++ js/squareRoot.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function squareRoot(number) {
+  if (number < 0) {
+    throw new RangeError("You can't find the square root of negative numbers")
+  }
+  return Math.sqrt(number);
+};
+
+test('square root of 4 is 2', () => {
+  expect(squareRoot(4)).toBe(2);
+});
